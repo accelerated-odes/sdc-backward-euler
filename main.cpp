c61a46f0c1332c92e080cced6443b1c5e2dd7b28
dwillcox <eugene.willcox@gmail.com>
2019-05-14 19:05:17 -0400

    Make use adaptive timestepping an option.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void do_sdc_kernel(Real* y_initial, Real* y_final,
 		   Real start_time, Real end_time, Real start_timestep,
                    Real tolerance, size_t maximum_newton_iters, 
 		   bool fail_if_maximum_newton, Real maximum_steps,
-		   Real epsilon, size_t size) {
+		   Real epsilon, size_t size, bool use_adaptive_timestep) {
 
   typedef SdcIntegrator<SparseLinearSolver,SystemClass,order> SdcIntClass;
 
@@ -46,7 +46,7 @@ void do_sdc_kernel(Real* y_initial, Real* y_final,
 			    start_time, end_time, start_timestep,
 			    tolerance, maximum_newton_iters, 
 			    fail_if_maximum_newton, maximum_steps,
-			    epsilon);
+			    epsilon, use_adaptive_timestep);
 
     for (size_t i = 0; i < maximum_steps; i++) {
       SdcIntClass::prepare(sdc);
@@ -68,7 +68,7 @@ void do_sdc_host(Real* y_initial, Real* y_final,
 		 Real start_time, Real end_time, Real start_timestep,
 		 Real tolerance, size_t maximum_newton_iters, 
 		 bool fail_if_maximum_newton, Real maximum_steps,
-		 Real epsilon, size_t size) {
+		 Real epsilon, size_t size, bool use_adaptive_timestep) {
 
   typedef SdcIntegrator<SparseLinearSolver,SystemClass,order> SdcIntClass;
 
@@ -87,7 +87,7 @@ void do_sdc_host(Real* y_initial, Real* y_final,
 			    start_time, end_time, start_timestep,
 			    tolerance, maximum_newton_iters, 
 			    fail_if_maximum_newton, maximum_steps,
-			    epsilon);
+			    epsilon, use_adaptive_timestep);
 
     for (size_t i = 0; i < maximum_steps; i++) {
       SdcIntClass::prepare(sdc);
@@ -110,7 +110,8 @@ int main(int argc, char* argv[]) {
   cudaProfilerStart();
 #endif
 
-  size_t num_systems = 128 * 128 * 128;
+  //  size_t num_systems = 128 * 128 * 128;
+  size_t num_systems = 1;
   const size_t order = 4;
 
   WallTimer timer;
@@ -144,12 +145,13 @@ int main(int argc, char* argv[]) {
 
   Real start_time = 0.0;
   Real end_time = 1.0;
-  Real start_timestep = (end_time - start_time)/10.0;
-  Real tolerance = 1.0e-6;
-  size_t maximum_newton_iters = 100;
+  Real start_timestep = (end_time - start_time)/5120.0;
+  Real tolerance = 1.0e-12;
+  size_t maximum_newton_iters = 1000;
   size_t maximum_steps = 1000000;
   bool fail_if_maximum_newton = true;
   Real epsilon = std::numeric_limits<Real>::epsilon();
+  bool use_adaptive_timestep = false;
 
   const int nThreads = 32;
   const size_t WarpBatchSize = 128;
@@ -164,7 +166,7 @@ int main(int argc, char* argv[]) {
 						    start_time, end_time, start_timestep,
 						    tolerance, maximum_newton_iters,
 						    fail_if_maximum_newton, maximum_steps,
-						    epsilon, num_systems);
+						    epsilon, num_systems, use_adaptive_timestep);
 #else
   do_sdc_kernel<SparseGaussJordan, 
 		VodeSystem, 
@@ -172,7 +174,7 @@ int main(int argc, char* argv[]) {
 					      start_time, end_time, start_timestep,
 					      tolerance, maximum_newton_iters,
 					      fail_if_maximum_newton, maximum_steps,
-					      epsilon, num_systems);
+					      epsilon, num_systems, use_adaptive_timestep);
 
   cuda_status = cudaDeviceSynchronize();
   assert(cuda_status == cudaSuccess);
@@ -182,6 +184,7 @@ int main(int argc, char* argv[]) {
 
   std::cout << std::endl << "Final Integration States -------------------" << std::endl;
   for (size_t i = 0; i < num_systems; i += VodeSystem::neqs) {
+    std::cout << std::setprecision(std::numeric_limits<Real>::digits10 + 1);
     std::cout << "y_final[" << i << "]: " << std::endl;
     std::cout << " ";
     for (size_t j = 0; j < VodeSystem::neqs; j++) {
