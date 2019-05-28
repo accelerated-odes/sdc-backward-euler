38753ed8dbf4321946e521c3069265be3355bbe5
Donald E. Willcox <eugene.willcox@gmail.com>
2019-05-27 20:56:37 -0700

    WIP - lots of vectorization work, added OpenMP versions of macros. vector example seems to work, but array4 interface needs work.

--- Examples/vector-math/main.cpp
+++ Examples/vector-math/main.cpp
@@ -4,39 +4,43 @@
 #include <cuda_profiler_api.h>
 #endif
 
+#include "VectorGpuMacros.H"
 #include "RealVectorSet.H"
+#include "VectorStorage.H"
 #include "WallTimer.H"
 
+template<size_t vector_set_length, size_t vector_length, class StorageType>
 #ifdef AMREX_USE_CUDA
-template<size_t vector_set_length, size_t vector_length>
 __global__
-void do_gpu_kernel(Real* y_initial, Real* y_final, size_t array_max_size) {
-  __shared__ RealVectorSet<vector_set_length, vector_length> x_initial, x_final;
-
-  x_initial.load(y_initial, array_max_size);
-  x_final = 0.0;
-  x_initial += 1.0;
-  x_initial *= 2.0;
-  x_initial /= 4.0;
-  x_initial *= 2.0;
-  x_final = x_initial;
-  x_final.save(y_final, array_max_size);
-}
 #endif
+void doit(Real* y_initial, Real* y_final, size_t array_comp_size,
+          int array_chunk_index = -1)
 
-template<size_t vector_set_length, size_t vector_length>
-void do_cpu_kernel(Real* y_initial, Real* y_final, size_t array_max_size) {
-  RealVectorSet<vector_set_length, vector_length> x_initial, x_final;
+{
+  PARALLEL_SHARED RealVectorSet<vector_set_length, vector_length, StorageType> x_initial;
+  PARALLEL_SHARED RealVectorSet<vector_set_length, vector_length, StorageType> x_final;
 
-  x_initial.load(y_initial, array_max_size);
-  x_final = 0.0;
-  x_initial += 1.0;
-  x_initial *= 2.0;
-  x_initial /= 4.0;
-  x_initial *= 2.0;
-  x_final = x_initial;
-  x_final.save(y_final, array_max_size);
+  PARALLEL_REGION
+    {
+      x_initial.map(y_initial, array_comp_size, array_chunk_index,
+                    0, 0, vector_set_length);
+      x_final.map(y_final, array_comp_size, array_chunk_index,
+                  0, 0, vector_set_length);
+
+      WORKER_SYNC();
+
+      x_final = 0.0;
+      x_initial += 1.0;
+      x_initial *= 2.0;
+      x_initial /= 4.0;
+      x_initial *= 2.0;
+      x_final = x_initial;
+
+      x_final.save(y_final, array_comp_size, array_chunk_index,
+                   0, 0, vector_set_length);
+    }
 }
+
 
 int main(int argc, char* argv[]) {
 
@@ -44,28 +48,28 @@ int main(int argc, char* argv[]) {
   cudaProfilerStart();
 #endif
 
-  const size_t N = 5120;
+  const int N = 5120;
 
   WallTimer timer;
 
   Real* y_initial;
   Real* y_final;
 
-#ifndef AMREX_USE_CUDA
   y_initial = new Real[N];
   y_final = new Real[N];
-#else
+
+#ifdef AMREX_USE_CUDA
   cudaError_t cuda_status = cudaSuccess;
   void* vp;
-  cuda_status = cudaMallocManaged(&vp, sizeof(Real) * N);
+  cuda_status = cudaMalloc(&vp, sizeof(Real) * N);
   assert(cuda_status == cudaSuccess);
 
-  y_initial = static_cast<Real*>(vp);
+  Real* y_initial_d = static_cast<Real*>(vp);
 
-  cuda_status = cudaMallocManaged(&vp, sizeof(Real) * N);
+  cuda_status = cudaMalloc(&vp, sizeof(Real) * N);
   assert(cuda_status == cudaSuccess);
 
-  y_final = static_cast<Real*>(vp);
+  Real* y_final_d = static_cast<Real*>(vp);
 #endif
 
   // initialize systems
@@ -73,46 +77,69 @@ int main(int argc, char* argv[]) {
     y_initial[i] = 1.0;
   }
 
+#ifdef AMREX_USE_CUDA
+  cuda_status = cudaMemcpy(y_initial_d, y_initial, sizeof(Real) * N,
+                           cudaMemcpyHostToDevice);
+  assert(cuda_status == cudaSuccess);
+#endif
+
   // size of each vector and number of vector sets in a given shared memory structure
   const int nVectorSize = 128;   // use vectors of length 128
-  const int nVectorSets = 2;     // each vectorset contains 2 component vectors
+  const int nVectorSets = 10;      // each vectorset contains 2 component vectors
 
-  // how many threads per block to use
+  // how many threads per block to use for CUDA
   const int nThreads = 64;
 
   // each block operates on a number of elements M in the global array
   // M = nVectorSize * nVectorSets
   // so we need ceil(N/M) threadblocks
-  const int nBlocks = static_cast<int>(ceil(((double) N)/(double) nVectorSize * nVectorSets));
+  const int nBlocks = static_cast<int>(ceil(((double) N)/((double) nVectorSize * nVectorSets)));
+
+  const size_t size_per_component = static_cast<size_t>(N/nVectorSets);
 
   timer.start_wallclock();
 
-#ifndef AMREX_USE_CUDA
-  do_cpu_kernel<nVectorSets, N/nVectorSets>(y_initial, y_final, N);
-#else
-  do_gpu_kernel<nVectorSets, nVectorSize><<<nBlocks, nThreads>>>(y_initial, y_final, N);
+#ifdef AMREX_USE_CUDA
+  doit<nVectorSets, nVectorSize,
+       //StackMemory<Real, nVectorSize>><<<nBlocks, nThreads>>>(y_initial_d, y_final_d, size_per_component);
+       HeapMemoryWindow<Real>><<<nBlocks, nThreads>>>(y_initial_d, y_final_d, size_per_component);
   cuda_status = cudaDeviceSynchronize();
   assert(cuda_status == cudaSuccess);
+#else
+  for (int i = 0; i < nBlocks; i++) {
+    doit<nVectorSets, nVectorSize,
+         //StackMemory<Real, nVectorSize>>(y_initial, y_final, size_per_component, i);
+         HeapMemoryWindow<Real>>(y_initial, y_final, size_per_component, i);
+  }
 #endif
 
   timer.stop_wallclock();
 
-  std::cout << std::endl << "Final Vector -------------------" << std::endl;
+#ifdef AMREX_USE_CUDA
+  cuda_status = cudaMemcpy(y_final, y_final_d, sizeof(Real) * N,
+                           cudaMemcpyDeviceToHost);
+  assert(cuda_status == cudaSuccess);
+#endif
+
+  //  std::cout << std::endl << "Final Vector -------------------" << std::endl;
   Real ysum = 0.0;
   for (size_t i = 0; i < N; i++) {
-    std::cout << std::setprecision(std::numeric_limits<Real>::digits10 + 1);
-    std::cout << "y_final[" << i << "] = " << y_final[i] << std::endl;
+    //std::cout << std::setprecision(std::numeric_limits<Real>::digits10 + 1);
+    //  std::cout << "y_final[" << i << "] = " << y_final[i] << std::endl;
     ysum += y_final[i];
   }
 
   std::cout << "sum of y: " << ysum << std::endl;
+
+  if (ysum == N * 2.0)
+    std::cout << "Success!" << std::endl;
 
 #ifndef AMREX_USE_CUDA
   std::cout << "Finished execution on host CPU" << std::endl;
 #else
   std::cout << "Finished execution on device" << std::endl;
 #endif
-  
+
   std::cout << std::endl << "walltime (s): " << timer.get_walltime() << std::endl;
 
 #ifndef AMREX_USE_CUDA
