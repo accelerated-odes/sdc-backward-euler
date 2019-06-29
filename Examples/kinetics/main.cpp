c4d11808e924358b85e183853b4d3d4634e66c93
Donald E. Willcox <eugene.willcox@gmail.com>
2019-06-28 20:59:53 -0700

    WIP update integrator driver for vectorized integrator.

--- Examples/kinetics/main.cpp
+++ Examples/kinetics/main.cpp
@@ -7,101 +7,47 @@
 #include "SdcIntegrator.H"
 #include "SparseGaussJordan.H"
 #include "vode_system.H"
-#include "RealVector.H"
+#include "MathVector.H"
+#include "MathVectorSet.H"
+#include "VectorStorage.H"
+#include "VectorParallelUtil.H"
 #include "WallTimer.H"
 
 #ifdef AMREX_USE_CUDA
-template<class SparseLinearSolver, class SystemClass, size_t order>
+template<class SparseLinearSolver, class SystemClass, size_t order, size_t vector_length>
 __global__
-void do_sdc_kernel(Real* y_initial, Real* y_final, 
+void do_sdc_kernel(Real* y_initial, Real* y_final,
 		   Real start_time, Real end_time, Real start_timestep,
-                   Real tolerance, size_t maximum_newton_iters, 
+                   Real tolerance, size_t maximum_newton_iters,
 		   bool fail_if_maximum_newton, Real maximum_steps,
-		   Real epsilon, size_t size, bool use_adaptive_timestep) {
+		   Real epsilon, size_t size, bool use_adaptive_timestep)
+{
+  PARALLEL_SHARED SystemClass ode_system;
+  PARALLEL_SHARED SdcIntegrator<SparseLinearSolver, SystemClass, order, vector_length, StackCreate> sdc;
+  PARALLEL_SHARED MathVectorSet<Real, SystemClass::neqs, vector_length, HeapWindow<Real, vector_length>> y_ini, y_fin;
 
-  typedef SdcIntegrator<SparseLinearSolver,SystemClass,order> SdcIntClass;
+  PARALLEL_REGION
+    {
+      // map y_ini and y_fin to y_initial and y_final
+      y_ini.map(y_initial, size, -1, 0, 0, SystemClass::neqs);
+      y_fin.map(y_final, size, -1, 0, 0, SystemClass::neqs);
 
-  const size_t WarpBatchSize = 128;
-  const size_t WarpSize = 32;
-  size_t warp_batch_id = blockIdx.x * WarpBatchSize;
-  size_t global_index, local_index;
+      // initialize system and integrator
+      ode_system.initialize();
+      sdc.initialize(ode_system, y_ini,
+                     start_time, end_time, start_timestep,
+                     tolerance, maximum_newton_iters,
+                     fail_if_maximum_newton, maximum_steps,
+                     epsilon, use_adaptive_timestep);
 
-  SystemClass ode_system;
-  global_index = threadIdx.x + warp_batch_id;
+      // integrate
+      sdc.integrate();
 
-  if (global_index >= size) return;
-
-  for (local_index = threadIdx.x; local_index < WarpBatchSize && global_index < size; local_index += WarpSize) {
-    global_index = local_index + warp_batch_id;
-
-    SdcIntClass sdc;
-    RealVector<SystemClass::neqs> y_ini;
-
-    for (size_t i = 0; i < SystemClass::neqs; i++) {
-      y_ini.data[i] = y_initial[global_index * SystemClass::neqs + i];
+      // save results to y_fin
+      sdc.save(y_fin);
     }
-
-    sdc.set_jacobian_layout(ode_system);
-    sdc.initialize(y_ini, 
-                   start_time, end_time, start_timestep,
-                   tolerance, maximum_newton_iters, 
-                   fail_if_maximum_newton, maximum_steps,
-                   epsilon, use_adaptive_timestep);
-
-    for (size_t i = 0; i < maximum_steps; i++) {
-      sdc.prepare();
-      sdc.solve();
-      sdc.update();
-      if (sdc.is_finished()) break; // need to do this on a per-system basis internally with a mask perhaps?
-    }
-
-    RealVector<SystemClass::neqs>& y_fin = sdc.get_current_solution();
-    for (size_t i = 0; i < SystemClass::neqs; i++) {
-      y_final[global_index * SystemClass::neqs + i] = y_fin.data[i];
-    }
-  }
 }
 #endif
-
-template<class SparseLinearSolver, class SystemClass, size_t order>
-void do_sdc_host(Real* y_initial, Real* y_final, 
-		 Real start_time, Real end_time, Real start_timestep,
-		 Real tolerance, size_t maximum_newton_iters, 
-		 bool fail_if_maximum_newton, Real maximum_steps,
-		 Real epsilon, size_t size, bool use_adaptive_timestep) {
-
-  typedef SdcIntegrator<SparseLinearSolver,SystemClass,order> SdcIntClass;
-
-  SystemClass ode_system;
-
-  for (size_t global_index = 0; global_index < size; global_index++) {
-    SdcIntClass sdc;
-    RealVector<SystemClass::neqs> y_ini;
-
-    for (size_t i = 0; i < SystemClass::neqs; i++) {
-      y_ini.data[i] = y_initial[global_index * SystemClass::neqs + i];
-    }
-
-    SdcIntClass::set_jacobian_layout(sdc, ode_system);
-    SdcIntClass::initialize(sdc, y_ini, 
-			    start_time, end_time, start_timestep,
-			    tolerance, maximum_newton_iters, 
-			    fail_if_maximum_newton, maximum_steps,
-			    epsilon, use_adaptive_timestep);
-
-    for (size_t i = 0; i < maximum_steps; i++) {
-      SdcIntClass::prepare(sdc);
-      SdcIntClass::solve(sdc);
-      SdcIntClass::update(sdc);
-      if (SdcIntClass::is_finished(sdc)) break;
-    }
-
-    RealVector<SystemClass::neqs>& y_fin = SdcIntClass::get_current_solution(sdc);
-    for (size_t i = 0; i < SystemClass::neqs; i++) {
-      y_final[global_index * SystemClass::neqs + i] = y_fin.data[i];
-    }
-  }
-}
 
 
 int main(int argc, char* argv[]) {
@@ -125,7 +71,7 @@ int main(int argc, char* argv[]) {
   y_initial = new Real[VodeSystem::neqs * num_systems];
   y_final = new Real[VodeSystem::neqs * num_systems];
 #else
-  cudaError_t cuda_status = cudaSuccess;
+  cudaError_t cuda_status;
   void* vp;
   cuda_status = cudaMallocManaged(&vp, sizeof(Real) * VodeSystem::neqs * num_systems);
   assert(cuda_status == cudaSuccess);
@@ -138,16 +84,19 @@ int main(int argc, char* argv[]) {
   y_final = static_cast<Real*>(vp);
 #endif
 
-  // initialize systems
-  for (size_t i = 0; i < num_systems; i += VodeSystem::neqs) {
-    y_initial[i] = 1.0;
-    y_initial[i+1] = 0.0;
-    y_initial[i+2] = 0.0;
+  // initialize systems with a Fab data layout
+  for (size_t i = 0; i < num_systems * VodeSystem::neqs; i++) {
+    if (i/num_systems == 0)
+      y_initial[i] = 1.0;
+    else if (i/num_systems == 1)
+      y_initial[i] = 0.0;
+    else if (i/num_systems == 2)
+      y_initial[i] = 0.0;
   }
 
   Real start_time = 0.0;
   Real end_time = 1.0;
-  Real start_timestep = (end_time - start_time)/5120.0;
+  Real start_timestep = (end_time - start_time)/10.0;
   Real tolerance = 1.0e-12;
   size_t maximum_newton_iters = 1000;
   size_t maximum_steps = 1000000;
@@ -155,52 +104,48 @@ int main(int argc, char* argv[]) {
   Real epsilon = std::numeric_limits<Real>::epsilon();
   bool use_adaptive_timestep = false;
 
-  const int nThreads = 32;
-  const size_t WarpBatchSize = 128;
-  const int nBlocks = static_cast<int>(ceil(((double) num_systems)/(double) WarpBatchSize));
+  const int kernel_vector_length = 32 * 32;
+  const int nBlocks = static_cast<int>(ceil(((double) num_systems)/(double) kernel_vector_length));
+  const int nThreads = 128;
 
   std::cout << "Starting integration ..." << std::endl;
 
   timer.start_wallclock();
 
-#ifndef AMREX_USE_CUDA
-  do_sdc_host<SparseGaussJordan, VodeSystem, order>(y_initial, y_final,
-						    start_time, end_time, start_timestep,
-						    tolerance, maximum_newton_iters,
-						    fail_if_maximum_newton, maximum_steps,
-						    epsilon, num_systems, use_adaptive_timestep);
-#else
-  do_sdc_kernel<SparseGaussJordan, 
-		VodeSystem, 
-		order><<<nBlocks, nThreads>>>(y_initial, y_final,
-					      start_time, end_time, start_timestep,
-					      tolerance, maximum_newton_iters,
-					      fail_if_maximum_newton, maximum_steps,
-					      epsilon, num_systems, use_adaptive_timestep);
+// #ifndef AMREX_USE_CUDA
+//   do_sdc_host<SparseGaussJordan, VodeSystem, order>(y_initial, y_final,
+// 						    start_time, end_time, start_timestep,
+// 						    tolerance, maximum_newton_iters,
+// 						    fail_if_maximum_newton, maximum_steps,
+// 						    epsilon, num_systems, use_adaptive_timestep);
+// #else
+  do_sdc_kernel<SparseGaussJordan<VodeSystem, kernel_vector_length>,
+		VodeSystem,
+		order, kernel_vector_length><<<nBlocks, nThreads>>>(y_initial, y_final,
+                                                                    start_time, end_time, start_timestep,
+                                                                    tolerance, maximum_newton_iters,
+                                                                    fail_if_maximum_newton, maximum_steps,
+                                                                    epsilon, num_systems, use_adaptive_timestep);
 
   cuda_status = cudaDeviceSynchronize();
   assert(cuda_status == cudaSuccess);
-#endif
+// #endif
 
   timer.stop_wallclock();
 
   std::cout << std::endl << "Final Integration States -------------------" << std::endl;
-  for (size_t i = 0; i < num_systems; i += VodeSystem::neqs) {
+  for (size_t i = 0; i < num_systems * VodeSystem::neqs; i++) {
     std::cout << std::setprecision(std::numeric_limits<Real>::digits10 + 1);
-    std::cout << "y_final[" << i << "]: " << std::endl;
-    std::cout << " ";
-    for (size_t j = 0; j < VodeSystem::neqs; j++) {
-      std::cout << y_final[i + j] << " ";
-    }
-    std::cout << std::endl;
+    std::cout << "y_final[" << i << "]: " << y_final[i] << std::endl;
   }
+  std::cout << std::endl;
 
 #ifndef AMREX_USE_CUDA
   std::cout << "Finished execution on host CPU" << std::endl;
 #else
   std::cout << "Finished execution on device" << std::endl;
 #endif
-  
+
   std::cout << std::endl << "Integration walltime (s): " << timer.get_walltime() << std::endl;
 
 #ifndef AMREX_USE_CUDA
